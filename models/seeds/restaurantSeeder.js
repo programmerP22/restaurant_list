5f1cb50f8757393659126e7f4d9e206119ff4e32
programmerP22 <chiangcw19@gmail.com>
2022-10-14 19:22:15 +0800

    feat: add todo seeder

---
+++ models/seeds/restaurantSeeder.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+const Restaurant = require('../restaurant') // 載入 todo model
+const restaurantList = require("../../restaurants.json").results
+
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+  Restaurant.create(restaurantList)
+    .then(() => {
+      console.log("restaurantSeeder done!")
+      db.close()
+    })
+    .catch(err => console.log(err))
+})
