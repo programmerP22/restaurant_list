93023201d95ad4e58354cb0a8665b363a06ad545
programmerP22 <chiangcw19@gmail.com>
2022-10-15 16:51:45 +0800

    feat: add edit restaurant detail function

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+
 // require packages used in the project
 const express = require('express')
 const mongoose = require('mongoose') 
@@ -5,7 +7,9 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const Restaurant = require("./models/Restaurant")
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.set('useFindAndModify', false)
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true})
 
 // 取得資料庫連線狀態
 const db = mongoose.connection
@@ -41,17 +45,6 @@ app.get("/", (req, res) => {
     .catch(err => console.log(err))
 })
 
-//show detail
-app.get('/restaurants/:id', (req, res) => {
-  const id = req.params.id
-  Restaurant.findById(id)
-    .lean()
-    .then(restaurant => res.render("show", { restaurant }))
-    .catch(err => console.log(err))
-})
-
-
-
 // search
 app.get('/search', (req, res) => {
   if (!req.query.keyword) {
@@ -72,17 +65,41 @@ app.get('/search', (req, res) => {
     .catch(error => console.error(error))
 })
 
-
 //new webpage
 app.get("/restaurants/new", (req, res) => {
   res.render("new")
 })
 
-
-//add function
+//create function
 app.post("/restaurants", (req, res) => {
   Restaurant.create(req.body)
     .then(() => res.redirect("/"))
+    .catch(err => console.log(err))
+})
+
+//show detail webpage
+app.get('/restaurants/:id', (req, res) => {
+  const id = req.params.id
+  Restaurant.findById(id)
+    .lean()
+    .then(restaurant => res.render("show", { restaurant }))
+    .catch(err => console.log(err))
+})
+
+// edit webpage
+app.get("/restaurants/:id/edit", (req, res) => {
+  const id = req.params.id
+  Restaurant.findById(id)
+    .lean()
+    .then(restaurant => res.render("edit", { restaurant }))
+    .catch(err => console.log(err))
+})
+
+// edit function
+app.post("/restaurants/:id", (req, res) => {
+  const id = req.params.id
+  Restaurant.findByIdAndUpdate(id, req.body)
+    .then(()=> {res.redirect(`/restaurants/${id}`)})
     .catch(err => console.log(err))
 })
 
