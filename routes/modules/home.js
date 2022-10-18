72455d404db8905934bb327a2b49b849d18f6233
programmerP22 <chiangcw19@gmail.com>
2022-10-18 12:04:39 +0800

    feat: modify the path of files in modules to lowercase (const Restaurant = require('../../models/restaurant'))

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const Restaurant = require('../../models/Restaurant')
+const Restaurant = require('../../models/restaurant')
 const sortSelector = require('../../utilities/sortSelector');
 
 //homepage
