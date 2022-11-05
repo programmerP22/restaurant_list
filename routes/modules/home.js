d64427817a6749c8ee8908e22b41650932819f09
programmerP22 <chiangcw19@gmail.com>
2022-11-05 14:31:42 +0800

    Refactor: modify search function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -20,9 +20,10 @@ router.get('/search', (req, res) => {
     return res.redirect('/')
   }
   const keyword = req.query.keyword.trim().toLowerCase()
-
+  const userId = req.user._id
   //在find()方法裡，先加入 ＄OR 条件语句 讓搜尋字串只要符合name或category其中一項便可，然後再使用正則表達式 $regex，搜尋包含搜尋字串的餐廳，最後再設置 $options 為 $i，讓搜尋結果不區分大小寫。
   Restaurant.find({
+    userId,
     $or: [
       { name: { $regex: keyword, $options: 'i', } },
       { category: { $regex: keyword, $options: 'i', } },
